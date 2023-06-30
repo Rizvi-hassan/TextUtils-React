b9f2c4d82b685fe7d93456b4921c8294ab5e09b6
Md. Rizvi Hassan <mdrizvihassanansari@gmail.com>
2023-06-30 09:37:24 +0530

    final cange

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -47,7 +47,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active " aria-expanded='false' href= "#">
-                Home
+                Home-default
               </a>
             </li>
             <li className="nav-item">
